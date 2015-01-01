0b299b693eb49bdb34628690ec7f3a7b59af2ed1
foreversmart <njutree@gmail.com>
2015-01-01 18:54:18 +0800

    wrap msg in client

--- gonet/client.go
+++ gonet/client.go
@@ -8,29 +8,31 @@
 package gonet
 
 import (
+	"bytes"
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
 type GoClient struct {
-	sendChan chan string
+	sendChan    chan string
 	receiveChan chan Message
 }
 
-var ClientMap map[string] *GoClient
+var ClientMap map[string]*GoClient
 
 func ClientInit() {
-	ClientMap = make(map[string] *GoClient)
+	ClientMap = make(map[string]*GoClient)
 }
 
 /*
 	send message
 */
-func Send(message Message, callback func(msg string)){
+func Send(message Message, callback func(msg string)) {
 
 	remote := message.Addr
-	if ClientMap[remote]==nil {
+	if ClientMap[remote] == nil {
 		ClientMap[remote] = &GoClient{sendChan: make(chan string, 100), receiveChan: make(chan Message, 100)}
 		//client keeper
 		go ClientMap[remote].clientKeeper(remote)
@@ -41,7 +43,7 @@ func Send(message Message, callback func(msg string)){
 		go ClientMap[remote].ClientRead(callback)
 		ClientMap[remote].sendChan <- message.Content
 		fmt.Println("nil")
-	}else{
+	} else {
 		//
 		ClientMap[remote].sendChan <- message.Content
 		fmt.Println("not nil")
@@ -56,13 +58,13 @@ func (goClient *GoClient) clientKeeper(remote string) {
 
 	defer func() { // 必须要先声明defer，否则不能捕获到panic异常
 		m := recover()
-        fmt.Println(m)
-        if err:=recover();err!=nil{
-            fmt.Println(err) // 这里的err其实就是panic传入的内容
-        }
-        sendChan <- "&**& quit this client &**&"//退出协程并且删掉主对象      
-    }()
-    
+		fmt.Println(m)
+		if err := recover(); err != nil {
+			fmt.Println(err) // 这里的err其实就是panic传入的内容
+		}
+		sendChan <- "&**& quit this client &**&" //退出协程并且删掉主对象
+	}()
+
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", remote)
 	checkError(err)
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
@@ -72,14 +74,14 @@ func (goClient *GoClient) clientKeeper(remote string) {
 	var msg = make([]byte, 1024)
 
 	// start send routine 降低读写冲突
-	go func (){
+	go func() {
 		for {
-			sendMsg := <- goClient.sendChan
+			sendMsg := <-goClient.sendChan
 			if sendMsg != "&**& quit this client &**&" {
 				fmt.Println("client sengding:", sendMsg)
 				_, err = conn.Write([]byte(sendMsg))
 				checkError(err)
-			}else {
+			} else {
 				//崩溃退出关闭发送协程
 				fmt.Println("quit client Keeper success")
 				goClient = nil //删除掉主对象
@@ -89,13 +91,13 @@ func (goClient *GoClient) clientKeeper(remote string) {
 	}()
 
 	//hear beat
-	go func () {
+	go func() {
 		for {
 			time.Sleep(time.Second * 60)
-			if (goClient !=nil){
+			if goClient != nil {
 				fmt.Println("heart")
 				goClient.sendChan <- "&**& !heart beat! &**&"
-			}else{
+			} else {
 				return
 			}
 		}
@@ -113,17 +115,25 @@ func (goClient *GoClient) clientKeeper(remote string) {
 }
 
 func (goClient *GoClient) ClientRead(f func(msg string)) {
+	tempbytes := bytes.Buffer{}
 	for {
-		tempMsg := <- goClient.receiveChan
-		fmt.Println("client get:", tempMsg.Content)
+		tempMsg := <-goClient.receiveChan
+		// fmt.Println("client get:", tempMsg.Content)
 		/*
 			user custom function deal message when the client
 
 			receive message
 		*/
-		f(tempMsg.Content)
+		tempbytes.WriteString(tempMsg.Content)
+		if strings.Count(tempbytes.String(), "&**&") >= 2 {
+			tempArr := strings.SplitN(tempbytes.String(), "&**&", 3)
+			f(tempArr[1])
+			tempbytes = bytes.Buffer{}
+			tempbytes.WriteString(tempArr[3])
+		} else {
+
+		}
 		// tempMsg.Content = tempMsg.Content + "2"
 		// Send(tempMsg)
 	}
 }
-
