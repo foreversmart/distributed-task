c992ea5c44fc9ac2548cd679fd02d6ae3f5aec56
foreversmart <njutree@gmail.com>
2015-01-02 17:33:43 +0800

    remove print reduce

--- scheduler/reduce.go
+++ scheduler/reduce.go
@@ -7,33 +7,30 @@
 */
 package scheduler
 
-import(
-	"distributed-task/gonet"
+import (
 	"distributed-task/gocommand"
+	"distributed-task/gonet"
 	"time"
-	"fmt"
 )
-
 
 var ReduceMap string
 var reduceChan chan string
 
 type UserReduceFunc func(rm, res string) string
 
-func InitReducer(userReduce UserReduceFunc){
+func InitReducer(userReduce UserReduceFunc) {
 	reduceChan = make(chan string, 100)
 	go reduceResult(userReduce)
 }
 
-func reduceResult(userReduce UserReduceFunc){
-	for{
+func reduceResult(userReduce UserReduceFunc) {
+	for {
 		select {
-		case res := <- reduceChan:
+		case res := <-reduceChan:
 			ReduceMap = userReduce(ReduceMap, res)
-			fmt.Println("ReduceMap", ReduceMap)
-		case <- time.After(time.Second * 20):
+		case <-time.After(time.Second * 20):
 			//30 秒没有更新数据reduce data to client
-			if ReduceMap!= ""{
+			if ReduceMap != "" {
 				reduceToClient()
 			}
 		}
@@ -44,7 +41,7 @@ func reduceResult(userReduce UserReduceFunc){
 	call by server
 	to send the res to client
 */
-func reduceToClient(){
+func reduceToClient() {
 	tempMap := make(map[string]string)
 	tempMap["result"] = ReduceMap
 	command := &gocommand.Command{"reduce", "reduce", tempMap}
